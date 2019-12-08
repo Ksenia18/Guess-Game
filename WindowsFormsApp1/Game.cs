fdd4b94a665288421d6a44f281e0db4919163c8f
Ksenia18 <astridbarmakova@yandex.ru>
2019-12-08 12:51:37 +0300

    Word Game
    Guess Word Game

---
+++ WindowsFormsApp1/Game.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Game : Form
+    {
+        public Game()
+        {
+            InitializeComponent();
+        }
+
+        private void Game_Load(object sender, EventArgs e)
+        {
+            string[] words;
+            string current_word = "";
+            string show_text = "";
+            int count_success = 0;
+            int count_spaces = 0;
+            int lives;
+            PictureBox[] lives_image;
+            void start_game(RichTextBox wordArea)
+            {
+                
+            }
+            string get_show_text(string word);
+            {
+                return " ";
+            }
+            void centering_text(RichTextBox word_area)
+            {
+
+            }
+            string is_contains(string word)
+            {
+                return "";
+            }
+            string get_new_show_text(string word)
+            {
+                return "";
+            }
+            void button_symbol_click(char symbol)
+            {
+
+            }
+        }
+    }
+}
